57da66eecf448faa654fec7524eaa5e4915e7aec
David Yahalomi <davidyaha@gmail.com>
2016-08-10 18:27:13 +0300

    * Updated dependency on graphql-tools to version 0.6.3

--- check-npm.js
+++ check-npm.js
@@ -3,5 +3,5 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
   'apollo-server': '^0.1.1',
   'express': '^4.13.4',
-  'graphql-tools': '^0.6.1',
+  'graphql-tools': '^0.6.3',
 }, 'davidyaha:apollo-accounts-server');
